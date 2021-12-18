c28868ecf66de56d269ef32348cf4aef1467ef9b
Jose Victor <josevictorferreira@outlook.com>
2021-12-17 22:49:49 -0300

    added storage file

---
+++ lib/storage.ex
@@ -0,0 +1,32 @@
+
+defmodule Storage do
+  alias :mnesia, as: Mnesia
+
+  def init do
+    setup()
+  end
+
+  defp setup do
+    Mnesia.create_schema([node()])
+    Mnesia.start()
+    create_table()
+  end
+
+  defp attributes do
+    [:id, :title, :link, :published_at, :description, :content]
+  end
+
+  defp create_table do
+    case Mnesia.create_table(Article, [attributes: attributes]) do
+      {:atomic, :ok} ->
+        Mnesia.add_table_index(Article, :ẗitle)
+      {:aborted, {:already_exists, Article}} ->
+        case Mnesia.table_info(Article, :attributes) do
+          [:id, :title, :link, :published_at, :description, :content] ->
+            :ok
+          other ->
+            {:error, other}
+        end
+    end
+  end
+end
